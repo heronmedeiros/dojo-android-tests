d8712b96f34854ea735365ff5ebe2d51c9910816
Tiago Justino <tiago.vieira@ifce.edu.br>
2013-02-20 14:02:54 -0300

    using InjectMocks

--- src/com/example/dojo1/test/SumTest.java
+++ src/com/example/dojo1/test/SumTest.java
@@ -1,6 +1,5 @@
 package com.example.dojo1.test;
 
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import java.security.InvalidParameterException;
@@ -8,6 +7,7 @@ import java.security.InvalidParameterException;
 import junit.framework.TestCase;
 
 import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.mockito.MockitoAnnotations.Mock;
 
 import com.example.dojo1.Convert;
@@ -17,63 +17,31 @@ import com.example.dojo1.Sum;
 
 public class SumTest extends TestCase {
 
-	
-	private Sum sum;
-   
 	@Mock
 	private Convert convert;
-	
 	@InjectMocks
-	private Sum sum1;
+	private Sum sum = new Sum();
 
     @Override
     protected void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(convert.binaryToDecimal("0")).thenReturn(0);
+        when(convert.binaryToDecimal("1")).thenReturn(1);
+        when(convert.binaryToDecimal("11")).thenReturn(3);
         super.setUp();
-        sum = new Sum();
     }
 
     
     public void testBinarySumWhen0And0Return0() {
-    	
-    	
-    	when(convert.binaryToDecimal("0")).thenReturn(12);
-    	
-    	sum1 = new Sum();
-    
-    	
-    	assertEquals(12, sum1.binarySum("0", "0"));
- 
-    
-/*    public void testBinarySumWhen0And0Return0() {
-    	Sum spySum = spy(new Sum());
-    	
-    	Convert mockConvert = mock(Convert.class);
-    	
-    	when(mockConvert.binaryToDecimal("0")).thenReturn(10);
-//    	when(mockConvert.binaryToDecimal("1")).thenReturn(1);
-    	
-    	assertEquals(2, spySum.binarySum("0", "0"));
-*/    	
-    	
-    	
-//        Convert convert = mock(Convert.class);        
-//        
-//        //Sum s = spy(new Sum());        
-//        
-//        when(convert.binaryToDecimal("0")).thenReturn(111);
-//        
-//        
-//        //int result = s.binarySum("0", "0");
-////        assertEquals(0, result);
-//        assertEquals(111, convert.binaryToDecimal("0"));
+    	assertEquals(0, sum.binarySum("0", "0"));
     }
 
     public void testBinarySumWhen1And11Return4() {
-        int result = sum.binarySum("1", "11");
-        assertEquals(4, result);
+        assertEquals(4, sum.binarySum("1", "11"));
     }
 
     public void testInvalidParameters() {
+        sum.setConvert(new Convert());
         try {
             sum.binarySum("1", "21");
             fail("Should raise a InvalidParameterException");
